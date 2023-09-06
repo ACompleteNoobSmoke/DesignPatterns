cd410c38c664e33e836eb2979b393c221c6ff2ac
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2023-09-06 07:19:13 -0500

    First Commit

---
+++ src/main/java/Memento/Organizer.java
@@ -0,0 +1,23 @@
+package Memento;
+
+public class Organizer {
+    private String state;
+
+    public void setState(String state){
+        this.state = state;
+    }
+
+    public String getState(){ return state; }
+
+    public Memento createMemento(String state) {
+        return new Memento(state);
+    }
+
+    public void restore(Memento memento){
+        this.state = memento.getState();
+    }
+
+    public String toString(){
+        return "State: " + state;
+    }
+}
